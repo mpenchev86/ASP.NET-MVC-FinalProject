92c7e376a10464d28b0d4bf7541ff9c0b99a2cda
mpenchev86 <peace.glory@yahoo.com>
2016-02-03 19:45:06 +0200

    Solution created; push.sh script file added

---
+++ Source/Web/MvcProject.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcProject.Web.Startup))]
+namespace MvcProject.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
