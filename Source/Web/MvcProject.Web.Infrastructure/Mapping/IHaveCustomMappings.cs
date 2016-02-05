5fe10150671102c899c553cd70e92430c4fefdd0
mpenchev86 <peace.glory@yahoo.com>
2016-02-05 22:36:38 +0200

    AutoMapper integrated with MvcProject.Web; MvcProject.Web.Infrastructure project created

---
+++ Source/Web/MvcProject.Web.Infrastructure/Mapping/IHaveCustomMappings.cs
@@ -0,0 +1,9 @@
+﻿namespace MvcProject.Web.Infrastructure.Mapping
+{
+    using AutoMapper;
+
+    public interface IHaveCustomMappings
+    {
+        void CreateMappings(IConfiguration configuration);
+    }
+}
---
+++ Source/Web/MvcProject.Web.Infrastructure/Mapping/IMapFrom.cs
@@ -0,0 +1,6 @@
+﻿namespace MvcProject.Web.Infrastructure.Mapping
+{
+    public interface IMapFrom<T>
+    {
+    }
+}
