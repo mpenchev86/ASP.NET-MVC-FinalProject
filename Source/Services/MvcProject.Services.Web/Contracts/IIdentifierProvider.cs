baa32f0e4ebdd646edb435a185bea8245af52d00
mpenchev86 <peace.glory@yahoo.com>
2016-02-24 22:27:38 +0200

    IdentifierProvider service created

---
+++ Source/Services/MvcProject.Services.Web/Contracts/IIdentifierProvider.cs
@@ -0,0 +1,15 @@
+﻿namespace MvcProject.Services.Web
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IIdentifierProvider
+    {
+        int DecodeId(string urlId);
+
+        string EncodeId(int id);
+    }
+}
---
+++ Source/Services/MvcProject.Services.Web/IdentifierProvider.cs
@@ -0,0 +1,25 @@
+﻿namespace MvcProject.Services.Web
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class IdentifierProvider : IIdentifierProvider
+    {
+        private const string Salt = ".443222";
+
+        public int DecodeId(string urlId)
+        {
+            var base64EncodedBytes = Convert.FromBase64String(urlId);
+            return int.Parse(Encoding.UTF8.GetString(base64EncodedBytes));
+        }
+
+        public string EncodeId(int id)
+        {
+            var plainTextBytes = Encoding.UTF8.GetBytes(id.ToString() + Salt);
+            return Convert.ToBase64String(plainTextBytes);
+        }
+    }
+}
