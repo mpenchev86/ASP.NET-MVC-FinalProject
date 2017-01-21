4a0cbd51435e6044f1762ffefedc75269ae700b5
mpenchev86 <peace.glory@yahoo.com>
2017-01-21 17:07:16 +0200

    model binder for order items' vote editor model implemented; ajax order product vote casting fixed

--- Source/Web/JustOrderIt.Web/Areas/Public/App_Start/VoteEditorModelBinder.cs
+++ Source/Web/JustOrderIt.Web/Areas/Public/App_Start/VoteEditorModelBinder.cs
@@ -17,12 +17,14 @@
             {
                 HttpRequestBase request = controllerContext.HttpContext.Request;
 
+                int modelId = Convert.ToInt32(request.Form.Get(request.Form.AllKeys.FirstOrDefault(k => k.Contains("ModelId"))));
                 int productId = Convert.ToInt32(request.Form.Get(request.Form.AllKeys.FirstOrDefault(k => k.Contains("ProductId"))));
                 string userId = request.Form.Get(request.Form.AllKeys.FirstOrDefault(k => k.Contains("UserId")));
                 double voteValue = Convert.ToDouble(request.Form.Get(request.Form.AllKeys.FirstOrDefault(k => k.Contains("VoteValue"))));
 
                 return new VoteEditorModel
                 {
+                    Id = modelId,
                     ProductId = productId,
                     UserId = userId,
                     VoteValue = voteValue,
