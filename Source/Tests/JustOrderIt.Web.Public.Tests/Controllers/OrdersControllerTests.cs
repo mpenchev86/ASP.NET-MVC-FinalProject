b43ba57c7a970e680c0888a8841decf526045522
mpenchev86 <peace.glory@yahoo.com>
2017-01-27 22:48:07 +0200

    orders tests

--- Source/Tests/JustOrderIt.Web.Public.Tests/Controllers/OrdersControllerTests.cs
+++ Source/Tests/JustOrderIt.Web.Public.Tests/Controllers/OrdersControllerTests.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Net;
     using System.Security.Principal;
     using System.Text;
     using System.Threading.Tasks;
@@ -39,7 +40,6 @@
             // Arrange
             var userName = "jkhfjksdf";
             var cart = this.GetShoppingCart(userName);
-            //var sessionKey = "jkghkhsfjhs";
 
             var identity = new Mock<IIdentity>();
             identity.Setup(x => x.Name).Returns(userName);
@@ -49,13 +49,11 @@
 
             var session = new Mock<HttpSessionStateBase>();
             session.SetupGet(x => x[It.IsAny<string>()]).Returns(cart);
-            //session.SetupSet(x => { x[It.IsAny<string>()] = cart; });
 
             // Act
             var controller = new OrdersController(this.identifierProvider.Object, this.orderItemsService.Object, this.ordersService.Object, this.productsService.Object, this.mappingService.Object);
             controllerContext.SetupGet(x => x.HttpContext.User).Returns(principal.Object);
             controllerContext.SetupGet(x => x.HttpContext.Session).Returns(session.Object);
-            //controllerContext.SetupGet(x => x.HttpContext.Session[sessionKey]).Returns(cart);
             controller.ControllerContext = controllerContext.Object;
 
             // Assert
@@ -64,6 +62,13 @@
                 .WithModel<ShoppingCartViewModel>(vm => 
                 {
                     Assert.IsNotNull(vm);
+                    Assert.AreEqual(vm, cart);
+                    Assert.AreEqual(vm.TotalCost, cart.TotalCost);
+                    Assert.AreEqual(vm.UserName, cart.UserName);
+                    CollectionAssert.AllItemsAreNotNull(vm.CartItems);
+                    Assert.AreEqual(vm.CartItems.Count, cart.CartItems.Count);
+                    CollectionAssert.AreEquivalent(vm.CartItems, cart.CartItems);
+                    CollectionAssert.AreEquivalent(vm.CartItems.Select(x => x.Product), cart.CartItems.Select(x => x.Product));
                 });
         }
 
@@ -89,7 +94,6 @@
             var controller = new OrdersController(this.identifierProvider.Object, this.orderItemsService.Object, this.ordersService.Object, this.productsService.Object, this.mappingService.Object);
             controllerContext.SetupGet(x => x.HttpContext.User).Returns(principal.Object);
             controllerContext.SetupGet(x => x.HttpContext.Session).Returns(session.Object);
-            //controllerContext.SetupGet(x => x.HttpContext.Session[sessionKey]).Returns(null);
             controller.ControllerContext = controllerContext.Object;
 
             // Assert
@@ -98,7 +102,71 @@
                 .WithModel<ShoppingCartViewModel>(vm =>
                 {
                     Assert.IsNotNull(vm);
+                    CollectionAssert.AllItemsAreNotNull(vm.CartItems);
+                    Assert.AreEqual(vm.TotalCost, cart.TotalCost);
+                    Assert.AreEqual(vm.UserName, cart.UserName);
                 });
+        }
+
+        [Test]
+        public void UpdateShoppingCartWorksCorrectlyWhenModelAndModelStateAreValid()
+        {
+            // Arrange
+            var cart = this.GetShoppingCart("jkfhsjd");
+            var session = new Mock<HttpSessionStateBase>();
+            session.SetupGet(x => x[It.IsAny<string>()]).Returns(null);
+            session.SetupSet(x => { x[It.IsAny<string>()] = cart; });
+            var controllerContext = new Mock<ControllerContext>();
+
+            // Act
+            var controller = new OrdersController(this.identifierProvider.Object, this.orderItemsService.Object, this.ordersService.Object, this.productsService.Object, this.mappingService.Object);
+            controllerContext.SetupGet(x => x.HttpContext.Session).Returns(session.Object);
+            controller.ControllerContext = controllerContext.Object;
+
+            // Assert
+            controller.WithCallTo(x => x.UpdateShoppingCart(cart))
+                .ShouldRenderPartialView("ShoppingCartPartial")
+                .WithModel<ShoppingCartViewModel>(vm => 
+                {
+                    Assert.IsNotNull(vm);
+                    Assert.AreEqual(vm, cart);
+                    Assert.AreEqual(vm.TotalCost, cart.TotalCost);
+                    Assert.AreEqual(vm.UserName, cart.UserName);
+                    CollectionAssert.AllItemsAreNotNull(vm.CartItems);
+                    CollectionAssert.AreEquivalent(vm.CartItems, cart.CartItems);
+                    Assert.IsTrue(vm.CartItems.All(x => x.ToDelete == false && x.ProductQuantity > 0));
+                    //Assert.IsTrue(vm.CartItems.All(x => x.ProductQuantity > 0));
+                });
+        }
+
+        [Test]
+        public void UpdateShoppingCartThrowsWhenModelIsInValid()
+        {
+            // Arrange
+            var session = new Mock<HttpSessionStateBase>();
+            
+            // Act
+            var controller = new OrdersController(this.identifierProvider.Object, this.orderItemsService.Object, this.ordersService.Object, this.productsService.Object, this.mappingService.Object);
+            
+            // Assert
+            var ex = Assert.Throws<HttpException>(() => controller.UpdateShoppingCart(null));
+            Assert.AreEqual((int)HttpStatusCode.BadRequest, ex.GetHttpCode());
+        }
+
+        [Test]
+        public void UpdateShoppingCartThrowsWhenModelStateIsInValid()
+        {
+            // Arrange
+            var cart = this.GetShoppingCart("jkfhsjd");
+            var session = new Mock<HttpSessionStateBase>();
+
+            // Act
+            var controller = new OrdersController(this.identifierProvider.Object, this.orderItemsService.Object, this.ordersService.Object, this.productsService.Object, this.mappingService.Object);
+            controller.ModelState.AddModelError(string.Empty, It.IsAny<string>());
+
+            // Assert
+            var ex = Assert.Throws<HttpException>(() => controller.UpdateShoppingCart(cart));
+            Assert.AreEqual((int)HttpStatusCode.BadRequest, ex.GetHttpCode());
         }
 
         #region Helpers
@@ -137,7 +205,7 @@
                         ToDelete = false
                     },
                 },
-                TotalCost = 872m,
+                //TotalCost = 872m,
             };
         }
         #endregion
