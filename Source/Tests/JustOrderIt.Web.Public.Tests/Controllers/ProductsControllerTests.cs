673e11bcfa3557e5416a28dfd40c5a636cacd029
mpenchev86 <peace.glory@yahoo.com>
2017-01-29 02:25:32 +0200

    products controller tests added

--- Source/Tests/JustOrderIt.Web.Public.Tests/Controllers/ProductsControllerTests.cs
+++ Source/Tests/JustOrderIt.Web.Public.Tests/Controllers/ProductsControllerTests.cs
@@ -2,9 +2,13 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Collections.Specialized;
     using System.Linq;
+    using System.Net;
     using System.Text;
     using System.Threading.Tasks;
+    using System.Web;
+    using System.Web.Mvc;
     using Areas.Public.Controllers;
     using Areas.Public.ViewModels.Comments;
     using Areas.Public.ViewModels.Products;
@@ -52,8 +56,119 @@
                 {
                     Assert.IsNotNull(vm);
                     Assert.AreEqual(vm, viewModel);
-                }
-                );
+                });
+        }
+
+        [Test]
+        public void SneakPeekShouldReturnMessageContentIfRequestIsNotAjax()
+        {
+            // Arrange
+            var requestMock = new Mock<HttpRequestBase>();
+            requestMock.Setup(x => x.Headers).Returns(new NameValueCollection());
+            requestMock.SetupGet(x => x.AppRelativeCurrentExecutionFilePath).Returns(It.IsAny<string>());
+            var responseMock = new Mock<HttpResponseBase>();
+            responseMock.SetupGet(x => x.StatusCode).Returns(/*(int)HttpStatusCode.Forbidden*/It.IsAny<int>());
+            responseMock.SetupSet(x => x.StatusCode = It.IsAny<int>());
+            var controllerContextMock = new Mock<ControllerContext>();
+            controllerContextMock.SetupGet(x => x.HttpContext.Request).Returns(requestMock.Object);
+            controllerContextMock.SetupGet(x => x.HttpContext.Response).Returns(responseMock.Object);
+
+            // Act
+            var controller = new ProductsController(this.productsService.Object, this.identifierProvider.Object, this.votesService.Object, this.mappingService.Object);
+            controller.ControllerContext = controllerContextMock.Object;
+
+            // Assert
+            //controller.WithCallTo(x => x.SneakPeek(It.IsAny<string>()))
+            //    .ShouldGiveHttpStatus(HttpStatusCode.Forbidden);
+
+            controller.WithCallTo(x => x.SneakPeek(It.IsAny<string>()))
+                .ShouldReturnContent(It.IsAny<string>());
+        }
+
+        [Test]
+        public void SneakPeekShouldReturnMessageContentIfProductIsNull()
+        {
+            // Arrange
+            var product = this.GetProduct();
+            this.identifierProvider.Setup(x => x.DecodeToIntId(It.IsAny<string>())).Returns(product.Id);
+            this.productsService.Setup(x => x.GetById(It.IsAny<int>())).Returns<Product>(null);
+            var requestMock = new Mock<HttpRequestBase>();
+            requestMock.Setup(x => x.Headers).Returns(new NameValueCollection());
+            requestMock.SetupGet(x => x.AppRelativeCurrentExecutionFilePath).Returns(It.IsAny<string>());
+            requestMock.Object.Headers.Add("X-Requested-With", "XMLHttpRequest");
+            var responseMock = new Mock<HttpResponseBase>();
+            responseMock.SetupGet(x => x.StatusCode).Returns(/*(int)HttpStatusCode.Forbidden*/It.IsAny<int>());
+            responseMock.SetupSet(x => x.StatusCode = It.IsAny<int>());
+            var controllerContextMock = new Mock<ControllerContext>();
+            controllerContextMock.SetupGet(x => x.HttpContext.Request).Returns(requestMock.Object);
+            controllerContextMock.SetupGet(x => x.HttpContext.Response).Returns(responseMock.Object);
+
+            // Act
+            var controller = new ProductsController(this.productsService.Object, this.identifierProvider.Object, this.votesService.Object, this.mappingService.Object);
+            controller.ControllerContext = controllerContextMock.Object;
+
+            // Assert
+            controller.WithCallTo(x => x.SneakPeek(It.IsAny<string>()))
+                .ShouldReturnContent(It.IsAny<string>());
+        }
+
+        [Test]
+        public void SneakPeekShouldReturnPartialViewResultIfProductExists()
+        {
+            // Arrange
+            var product = this.GetProduct();
+            this.identifierProvider.Setup(x => x.DecodeToIntId(It.IsAny<string>())).Returns(product.Id);
+            this.productsService.Setup(x => x.GetById(It.IsAny<int>())).Returns(product);
+            var viewModel = new ProductSneakPeekViewModel();
+            this.mappingService.Setup(x => x.Map<Product, ProductSneakPeekViewModel>(It.IsAny<Product>())).Returns(viewModel);
+            var requestMock = new Mock<HttpRequestBase>();
+            requestMock.Setup(x => x.Headers).Returns(new NameValueCollection());
+            requestMock.SetupGet(x => x.AppRelativeCurrentExecutionFilePath).Returns(It.IsAny<string>());
+            requestMock.Object.Headers.Add("X-Requested-With", "XMLHttpRequest");
+            var responseMock = new Mock<HttpResponseBase>();
+            responseMock.SetupGet(x => x.StatusCode).Returns(/*(int)HttpStatusCode.Forbidden*/It.IsAny<int>());
+            responseMock.SetupSet(x => x.StatusCode = It.IsAny<int>());
+            var controllerContextMock = new Mock<ControllerContext>();
+            controllerContextMock.SetupGet(x => x.HttpContext.Request).Returns(requestMock.Object);
+            controllerContextMock.SetupGet(x => x.HttpContext.Response).Returns(responseMock.Object);
+
+            // Act
+            var controller = new ProductsController(this.productsService.Object, this.identifierProvider.Object, this.votesService.Object, this.mappingService.Object);
+            controller.ControllerContext = controllerContextMock.Object;
+
+            // Assert
+            controller.WithCallTo(x => x.SneakPeek(It.IsAny<string>()))
+                .ShouldRenderDefaultPartialView()
+                .WithModel<ProductSneakPeekViewModel>(vm => 
+                {
+                    Assert.IsNotNull(vm);
+                    Assert.AreEqual(vm, viewModel);
+                });
+        }
+
+        #region Helpers
+        private void PrepareController()
+        {
+            this.autoMapperConfig = new AutoMapperConfig();
+            autoMapperConfig.Execute(typeof(ProductsController).Assembly);
+            this.productsService = new Mock<IProductsService>();
+            this.votesService = new Mock<IVotesService>();
+            this.identifierProvider = new Mock<IIdentifierProvider>();
+            this.mappingService = new Mock<IMappingService>();
+        }
+
+        public ProductFullViewModel GetProductViewModel()
+        {
+            return new ProductFullViewModel()
+            {
+                CommentsWithRatings = new List<CommentWithRatingViewModel>()
+                {
+                    new CommentWithRatingViewModel() { Id = 1, CommentCreatedOn = DateTime.Now, CommentContent = "jkkuiqhjvcjhbmqkwhkjkukgausu", UserName = "bcvqiboqq", Rating = 2 },
+                    new CommentWithRatingViewModel() { Id = 3, CommentCreatedOn = DateTime.Now, CommentContent = "jkkuiqhjvcjhbmqkwhkjkukgausu", UserName = "bcvqiboqq", Rating = 4},
+                    new CommentWithRatingViewModel() { Id = 2, CommentCreatedOn = DateTime.Now, CommentContent = "jkkuiqhjvcjhbmqkwhkjkukgausu", UserName = "bcvqiboqq", },
+                    new CommentWithRatingViewModel() { Id = 5, CommentCreatedOn = DateTime.Now, CommentContent = "jkkuiqhjvcjhbmqkwhkjkukgausu", UserName = "bcvqiboqq", },
+                },
+            };
         }
 
         private Product GetProduct()
@@ -79,31 +194,6 @@
                         VoteValue = 3,
                         User = new ApplicationUser() { UserName = "qtyqrwtvdb" }
                     },
-                },
-            };
-        }
-
-        #region Helpers
-        private void PrepareController()
-        {
-            this.autoMapperConfig = new AutoMapperConfig();
-            autoMapperConfig.Execute(typeof(ProductsController).Assembly);
-            this.productsService = new Mock<IProductsService>();
-            this.votesService = new Mock<IVotesService>();
-            this.identifierProvider = new Mock<IIdentifierProvider>();
-            this.mappingService = new Mock<IMappingService>();
-        }
-
-        public ProductFullViewModel GetProductViewModel()
-        {
-            return new ProductFullViewModel()
-            {
-                CommentsWithRatings = new List<CommentWithRatingViewModel>()
-                {
-                    new CommentWithRatingViewModel() { Id = 1, CommentCreatedOn = DateTime.Now, CommentContent = "jkkuiqhjvcjhbmqkwhkjkukgausu", UserName = "bcvqiboqq", Rating = 2 },
-                    new CommentWithRatingViewModel() { Id = 3, CommentCreatedOn = DateTime.Now, CommentContent = "jkkuiqhjvcjhbmqkwhkjkukgausu", UserName = "bcvqiboqq", Rating = 4},
-                    new CommentWithRatingViewModel() { Id = 2, CommentCreatedOn = DateTime.Now, CommentContent = "jkkuiqhjvcjhbmqkwhkjkukgausu", UserName = "bcvqiboqq", },
-                    new CommentWithRatingViewModel() { Id = 5, CommentCreatedOn = DateTime.Now, CommentContent = "jkkuiqhjvcjhbmqkwhkjkukgausu", UserName = "bcvqiboqq", },
                 },
             };
         }
